a8b09eece83a5676b2b2e486a5d5a5270fc2cfa9
on vacation <293451@naver.com>
2022-09-12 15:21:57 +0900

    Design del other webpage
    and start elice

--- onepage2/Projects/Restaurant-Review-App/js/register-sw.js
+++
@@ -1,8 +0,0 @@
-// Register a service worker.
-if (navigator.serviceWorker) {
-  navigator.serviceWorker.register('/sw.js').then(function(reg) {
-    console.log("Service Worker registered");
-  }).catch((e) => {
-    console.log("Service Worker failed to register", e);
-  });
-}
