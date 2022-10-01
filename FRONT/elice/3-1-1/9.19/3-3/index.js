8bdecc7799389847ba4cae3ae61b382707732afe
on vacation <293451@naver.com>
2022-10-01 12:13:11 +0900

    delete elice course

--- FRONT/elice/3-1-1/9.19/3-3/index.js
+++
@@ -1,9 +0,0 @@
-function getOnlyEvenNumber(inputA){
-    var evenNumber=[];
-    for(let n of inputA){
-        if(n%2===0){
-            evenNumber.push(n);
-        }
-    }
-    return evenNumber;
-}
